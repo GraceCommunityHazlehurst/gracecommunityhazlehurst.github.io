5b3a56a1308ef76e19ab959c166c9a9e69a0928c
GraceCommunityHazlehurst <walt@9983.us>
2025-01-02 15:36:36 -0500

    Update javatari.js

--- javatari.js
+++ javatari.js
@@ -22,9 +22,9 @@ Javatari = {
 
     // General configuration
     AUTO_START:                     true,
-    AUTO_POWER_ON_DELAY:            3000,                       // -1: no auto Power-ON; >= 0: wait specified milliseconds before Power-ON
-    CARTRIDGE_SHOW_RECENT:          true,
-    CARTRIDGE_CHANGE_DISABLED:      false,
+    AUTO_POWER_ON_DELAY:            1200,                       // -1: no auto Power-ON; >= 0: wait specified milliseconds before Power-ON
+    CARTRIDGE_SHOW_RECENT:          false,
+    CARTRIDGE_CHANGE_DISABLED:      true,
     CARTRIDGE_LABEL_COLORS:         "",                         // Space-separated colors for Label, Background, Border. e.g. "#f00 #000 transparent". Leave "" for defaults
     SCREEN_RESIZE_DISABLED:         false,
     SCREEN_CONSOLE_PANEL_DISABLED:  false,
@@ -53,7 +53,7 @@ Javatari = {
     SERVER_KEEPALIVE:               0,
 
     RESET:                          0,                          // if value = 1 clear all saved data on the client
-    ALLOW_URL_PARAMETERS:           true                        // Allows user to override any of these parameters via URL query parameters
+    ALLOW_URL_PARAMETERS:           false                        // Allows user to override any of these parameters via URL query parameters
 
 };
 
